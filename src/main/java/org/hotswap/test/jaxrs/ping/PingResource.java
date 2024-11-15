567e0cdf5ff8dcc946408b0bd743f2fe4e9eed15
Francesco Nigro <nigro.fra@gmail.com>
2024-11-15 12:33:30 +0100

    Revert "Run PingResource api endpoints in event loop"
    This reverts commit 1ba59008e48bdfe7bb554a28a70c0eaaaead7a72.

--- src/main/java/org/hotswap/test/jaxrs/ping/PingResource.java
+++ src/main/java/org/hotswap/test/jaxrs/ping/PingResource.java
@@ -9,8 +9,6 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
-import io.smallrye.common.annotation.NonBlocking;
-
 @Path("ping")
 public class PingResource {
   private static final String template = "Hello, %s!";
@@ -21,7 +19,6 @@ public class PingResource {
   @GET
   @Produces({"text/plain"})
   @Path("simple")
-  @NonBlocking
   public Response getSimple() {
     return Response.ok().build();
   }
@@ -29,7 +26,6 @@ public class PingResource {
   @GET
   @Produces({"text/plain"})
   @Path("hello")
-  @NonBlocking
   public String hello() {
     return "helloJ";
   }
@@ -37,7 +33,6 @@ public class PingResource {
   @GET
   @Path("greeting")
   @Produces({"text/plain"})
-  @NonBlocking
   public String greeting(@QueryParam("name") String name) {
     //System.out.println((new SimpleDateFormat("HH:mm:ss.SSS")).format(new Date(System.currentTimeMillis())));
     String suffix = (name != null) ? name : "World";
